dedaba4c9e5797c4b18e31fde5d613e34a107464
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2023-03-25 00:36:50 +0430

    fixing loader

--- components/SearchLoader.jsx
+++ components/SearchLoader.jsx
@@ -1,12 +1,11 @@
-import spinner from "../public/spinner-loader.gif"
-
-
 export default function SearchLoader() {
     return(
         <>
             <div className="loader backgroundDiv">
-                <img src={"https://i.pinimg.com/originals/3e/f0/e6/3ef0e69f3c889c1307330c36a501eb12.gif"} />
+                <img src={"https://i.pinimg.com/originals/3e/f0/e6/3ef0e69f3c889c1307330c36a501eb12.gif"} loading="auto"/>
             </div>
         </>
     );
 }
+
+// i had this gift in my public file
